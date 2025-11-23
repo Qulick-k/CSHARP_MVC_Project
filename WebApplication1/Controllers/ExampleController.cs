40174fa270fc968ef861297b3438264af39a2226
qulick <58820014+Qulick-k@users.noreply.github.com>
2025-11-23 20:00:36 +0800

    屬性標籤基本觀念
    屬性標籤基本觀念

---
+++ WebApplication1/Controllers/ExampleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("example2")]
+    public class ExampleController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Index";
+        }
+
+        [HttpGet("delete/{id?}")]
+        public string Delete(int id)
+        {
+            return "Delete" + id;
+        }
+
+        [ActionName("delete")] //使用屬性標籤
+        [HttpPost]
+        public string DeletePost()
+        {
+            return "DeletePost";
+        }
+    }
+}
