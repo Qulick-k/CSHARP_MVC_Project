a7619033cc6c22e7f1b947746f9bfa07cdc1a2ef
qulick <58820014+Qulick-k@users.noreply.github.com>
2025-11-26 22:59:43 +0800

    取得GET參數及關鍵字搜尋
    取得GET參數及關鍵字搜尋

---
+++ WebApplication1/Controllers/News2Controller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Dtos;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class News2Controller : Controller
+    {
+        private readonly KcgContext _context;
+        public News2Controller(KcgContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(string keyword)
+        {
+            var result = from a in _context.News
+                         join b in _context.TOPMenu on a.NewsId equals b.TOPMenuId
+                         select new NewsDto
+                         {
+                             Click = a.Click,
+                             Enable = a.Enable,
+                             EndDateTime = a.EndDateTime,
+                             NewsId = a.NewsId,
+                             StartDateTime = a.StartDateTime,
+                             Title = a.Title,
+                             UpdateDateTime = a.UpdateDateTime,
+                             UpdateEmploeeName = b.Name
+                         };
+            if (!string.IsNullOrEmpty(keyword)) result = result.Where(x => x.Title.Contains(keyword));
+            return View(result.ToList());
+        }
+    }
+}
